5ed52498a4882090305979edaf557d5f47e021e7
Evan Burawa <eburawa@gmail.com>
2022-02-13 20:58:22 -0800

    updated connect.py and Server

--- Server.py
+++ Server.py
@@ -1,26 +1,82 @@
+from paramiko import SSHClient
+from scp import SCPClient
+
+
 class Server:
     
     def __init__(self, config):
       self.config = config
 
-      self.host = config.remote_server_ip
-      self.user = config.remote_server_user
-      self.pw = config.remote_server_pw
-      self.html_dir = config.remote_server_html_dir
+      self._host = config.remote_server_ip
+      self._user = config.remote_server_user
+      self._pw = config.remote_server_pw
+      self._html_dir = config.remote_server_html_dir
 
       print('Server loaded')
-    
-    def set_username(username):
-      return
-    
-    def set_host(host):
-      return
 
-    def set_password(password):
-      return
 
-    def set_local_dir(dir):
-      return
+    # host
+    @property
+    def host(self):
+        return self._host
+
+    @host.setter
+    def host(self, value):
+        self._host = value
+
+    @host.deleter
+    def host(self):
+        del self._host
+
+
+    # user
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, value):
+        self._user = value
+
+    @user.deleter
+    def user(self):
+        del self._user
+
+
+    # pw
+    @property
+    def pw(self):
+        return self._pw
+
+    @pw.setter
+    def pw(self, value):
+        self._pw = value
+
+    @pw.deleter
+    def pw(self):
+        del self._pw
+
+
+    # html_dir
+    @property
+    def html_dir(self):
+        return self._html_dir
+
+    @html_dir.setter
+    def html_dir(self, value):
+        self._html_dir = value
+
+    @html_dir.deleter
+    def html_dir(self):
+        del self._html_dir
+
+
+
+    ###############################
+    #
+    #   Methods
+    #
+    ###############################
 
     def connect():
       return
--- connect.py
+++ connect.py
@@ -1,19 +1,21 @@
 # Localhost
 LOCAL_DB_NAME = 'wordpress'
-LOCAL_DB_HOST='localhost'
-LOCAL_DB_USER='root'
-LOCAL_DB_PASSWORD=''
+LOCAL_DB_HOST ='localhost'
+LOCAL_DB_USER ='root'
+LOCAL_DB_PASSWORD ='root'
+
 
 
 # New Host Server
 REMOTE_SERVER_IP = ''
 REMOTE_SERVER_USER = '';
+REMOTE_SERVER_PASSWORD = '';
 REMOTE_SERVER_HTML_DIR = ''
 
 
 # New Host DB
-REMOTE_HOST_DB = ''
 REMOTE_DB_NAME = ''
 REMOTE_DB_HOST ='localhost'
 REMOTE_DB_USER ='root'
 REMOTE_DB_PASSWORD =''
+REMOTE_DB_PORT =''
